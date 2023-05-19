300a0864968c6fa2bbf123453d7f37caced1ef0a
tkellys <thamireskellysantos@gmail.com>
2023-05-19 19:17:48 -0300

    notas-patinhas

--- service/notas-service.js
+++ service/notas-service.js
@@ -1,57 +1,15 @@
-let trocaImagem = document.querySelectorAll(".pata-cinza");
+function aplicarNota(id, nota){
 
-document.addEventListener("click", function(e){
-    let pataColorida = e.target.classList;
-    if(!pataColorida.contains('rosa')){
-        trocaImagem.forEach(function(pata){
-            pata.classList.remove('rosa');
-        })
-        pataColorida.add('rosa')
+    /**Removendo todas as notas */   
+    for (let index = 1; index <= 5; index++) {
+        const imagem = document.getElementById(`${id}-pata-${index}`);
+        imagem.src = "../assets/img/patinha.jpg";
     }
-})
 
-
-
-
-
-
-// const trocaImagemNotaUm = document.querySelector("[data-nota]");
-
-// trocaImagemNotaUm.addEventListener("click", () => {
-//     trocar();
-// });
-
-// const trocaImagemNotaDois = document.querySelector("[data-nota]");
-
-// trocaImagemNotaDois.addEventListener("click", () => {
-//     trocar();
-// });
-
-// const trocaImagemNotaTres = document.querySelector("[data-nota]");
-
-// trocaImagemNotaTres.addEventListener("click", () => {
-//     trocar();
-// });
-
-// const trocaImagemNotaQuatro = document.querySelector("[data-nota]");
-
-// trocaImagemNotaQuatro.addEventListener("click", () => {
-//     trocar();
-// });
-
-// const trocaImagemNotaCinco = document.querySelector("[nota-cinco]");
-
-// trocaImagemNotaCinco.addEventListener("click", () => {
-//     trocar();
-// });
-
-// let trocaImagem = document.querySelector('.pata-cinza')
-// function trocar() {
-//     const imagem = trocaImagem.getAttribute("src");
-//     if (imagem === "../assets/img/patinha.jpg") {
-//         trocaImagem.setAttribute("src", "../assets/img/patinhaRosa.png");
-//     } else {
-//         trocaImagem.setAttribute("src", "../assets/img/patinhaRosa.jpg");
-//     }
-// }
-
+    /**Aplicando as notas de acordo com atributo nota */   
+    for (let index = 1; index <= nota; index++) {
+        const imagem = document.getElementById(`${id}-pata-${index}`);        
+        imagem.src = "../assets/img/patinhaRosa.jpg";
+    }   
+    
+}
